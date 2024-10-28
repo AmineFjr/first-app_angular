62a12682cb249f1b9c9f19d26e8db77d12b6096c
AmineFjr <fajry599@gmail.com>
2024-10-28 15:12:48 +0100

    feat: add form on detail

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,14 +1,11 @@
-import { Injectable } from '@angular/core';
-import {HousingLocation} from "./housinglocation";
-
+import {Injectable} from '@angular/core';
+import {HousingLocation} from './housinglocation';
 @Injectable({
   providedIn: 'root',
 })
-
 export class HousingService {
   readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-
-  housingLocationList: HousingLocation[] = [
+  protected housingLocationList: HousingLocation[] = [
     {
       id: 0,
       name: 'Acme Fresh Start Housing',
@@ -110,13 +107,15 @@ export class HousingService {
       laundry: true,
     },
   ];
-
-  constructor() { }
-
   getAllHousingLocations(): HousingLocation[] {
     return this.housingLocationList;
   }
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
+  }
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+        `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
+    );
   }
 }
